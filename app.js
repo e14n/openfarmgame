da3079101d5c9bf853e5f4e7569280d4d15bdf02
Evan Prodromou <evan@status.net>
2013-01-01 11:14:42 -0800

    Config file

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
+// app.js
+//
+// main function for open farm game
+//
+// Copyright 2013, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-/**
- * Module dependencies.
- */
+var fs = require("fs"),
+    path = require("path"),
+    _ = require("underscore"),
+    express = require('express'),
+    routes = require('./routes'),
+    config,
+    defaults = {
+        port: 4000,
+        address: "localhost",
+        hostname: "localhost"
+    },
+    app;
 
-var express = require('express')
-  , routes = require('./routes');
+if (fs.existsSync("/etc/openfarmgame.json")) {
+    config = _.defaults(JSON.parse(fs.readFileSync("/etc/openfarmgame.json")),
+                        defaults);
+} else {
+    config = defaults;
+}
 
-var app = module.exports = express.createServer();
+app = module.exports = express.createServer();
+
+config = _.defaults(config, defaults);
 
 // Configuration
 
@@ -31,6 +63,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(config.port, config.address, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
