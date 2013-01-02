648e2ea725b49660188751db6d77d2db6971bdad
Evan Prodromou <evan@status.net>
2013-01-02 04:21:47 -0800

    UI for planting a crop
    A page for planting a crop

--- app.js
+++ app.js
@@ -66,6 +66,8 @@ app.get('/login', routes.login);
 app.post('/login', routes.handleLogin);
 app.get('/about', routes.about);
 app.get('/farmer/:webfinger', routes.farmer);
+app.get('/plant/:webfinger/:plot', routes.plant);
+app.post('/plant/:webfinger/:plot', routes.handlePlant);
 
 app.listen(config.port, config.address, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -24,41 +24,80 @@ exports.handleLogin = function(req, res, next) {
 
     // Pretend they're logged in
 
-    res.redirect("/farmer/"+webfinger);
+    res.redirect("/farmer/"+webfinger, 303);
 };
 
 exports.farmer = function(req, res, next) {
 
     var webfinger = req.params.webfinger,
-        farmer = {
-            id: webfinger,
-            name: "Test Farmer",
-            coins: 10,
-            plots: [
-                {
-                    id: "tag:openfarmgame.com,2013:"+webfinger+":plot:1",
-                    crop: {
-                        id: "tag:openfarmgame.com,2013:"+webfinger+":crop:corn:1",
-                        name: "Corn",
-                        status: "New"
-                    }
-                },
-                {
-                    id: "tag:openfarmgame.com,2013:"+webfinger+":plot:2",
-                    crop: {
-                        id: "tag:openfarmgame.com,2013:"+webfinger+":crop:tomatoes:3",
-                        name: "Tomatoes",
-                        status: "Almost ready"
-                    }
-                },
-                {
-                },
-                {
-                },
-                {
-                }
-            ]
-        };
+        farmer = testFarmer(webfinger);
 
     res.render('farmer', { title: 'Farmer ' + farmer.name, farmer: farmer });
+};
+
+exports.plant = function(req, res, next) {
+    var webfinger = req.params.webfinger,
+        plot = req.params.plot,
+        farmer = testFarmer(webfinger),
+        crops = testCrops();
+
+    res.render('plant', { title: 'Plant a new crop', farmer: farmer, plot: plot, crops: crops });
+};
+
+exports.handlePlant = function(req, res, next) {
+    var webfinger = req.body.webfinger,
+        plot = req.body.plot,
+        farmer = testFarmer(webfinger),
+        crops = testCrops();
+
+    res.redirect("/farmer/"+webfinger, 303);
+};
+
+var testFarmer = function(webfinger) {
+    return {
+        id: webfinger,
+        name: "Test Farmer",
+        coins: 10,
+        plots: [
+            {
+                id: "tag:openfarmgame.com,2013:"+webfinger+":plot:1",
+                crop: {
+                    id: "tag:openfarmgame.com,2013:"+webfinger+":crop:corn:1",
+                    name: "Corn",
+                    status: "New",
+                    needsWater: true
+                }
+            },
+            {
+                id: "tag:openfarmgame.com,2013:"+webfinger+":plot:2",
+                crop: {
+                    id: "tag:openfarmgame.com,2013:"+webfinger+":crop:tomatoes:3",
+                    name: "Tomatoes",
+                    status: "Ready",
+                    ready: true
+                }
+            },
+            {
+            },
+            {
+            },
+            {
+            }
+        ]
+    };
+};
+
+var testCrops = function() {
+    return [
+        {
+            name: "Corn",
+            cost: 5,
+            price: 18
+        },
+        {
+            name: "Tomatoes",
+            cost: 3,
+            price: 10
+        }
+    ];
 };
