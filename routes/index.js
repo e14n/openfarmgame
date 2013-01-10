3b62c3031b9e1c73b7683091131301fe10f70310
Evan Prodromou <evan@status.net>
2013-01-10 00:12:57 -0500

    Move handleLogout out of the OAuth flow

--- routes/index.js
+++ routes/index.js
@@ -104,14 +104,6 @@ exports.handleLogin = function(req, res, next) {
     });
 };
 
-exports.handleLogout = function(req, res) {
-
-    delete req.session.farmerID;
-    delete req.user;
-
-    res.redirect("/", 303);
-};
-
 exports.authorized = function(req, res, next) {
 
     var hostname = req.params.hostname,
@@ -198,6 +190,14 @@ exports.authorized = function(req, res, next) {
             }
         }
     });
+};
+
+exports.handleLogout = function(req, res) {
+
+    delete req.session.farmerID;
+    delete req.user;
+
+    res.redirect("/", 303);
 };
 
 exports.farmer = function(req, res, next) {
